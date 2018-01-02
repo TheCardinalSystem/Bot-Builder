94e069c9849b901e4ee932092bdd7182d4976dea
Cardinal System <dudehenry.peace@gmail.com>
2018-01-02 18:52:03 -0500

    Updated some other things...

---
+++ src/com/Cardinal/BotCreator/Gui/Components/Cells/Default/EventCell.java
@@ -0,0 +1,46 @@
+package com.Cardinal.BotCreator.Gui.Components.Cells.Default;
+
+import javax.swing.ImageIcon;
+
+import com.Cardinal.BotCreator.AssetLibraries.ImageLibrary;
+import com.Cardinal.BotCreator.Gui.Components.Cells.ComboBoxCell;
+
+import sx.blah.discord.api.events.EventSubscriber;
+
+/**
+ * A {@link ComboBoxCell} used to represent {@link EventSubscriber
+ * EventSubscriber} instances.
+ * 
+ * @author Cardinal System
+ * @see EventCell#getName()
+ * @see EventCell#getIcon()
+ */
+public class EventCell implements ComboBoxCell {
+
+	/**
+	 * The unchanging icon used for this cell.
+	 * <img src="./doc-files/annotation_obj.png" />
+	 */
+	private static final ImageIcon ICON = new ImageIcon(ImageLibrary.ANNOTATION.getImage());
+
+	/**
+	 * The icon of this cell.
+	 * 
+	 * @see EventCell#ICON
+	 */
+	@Override
+	public ImageIcon getIcon() {
+		return ICON;
+	}
+
+	/**
+	 * The name of this cell.
+	 * 
+	 * @return "Event Handler"
+	 */
+	@Override
+	public String getName() {
+		return "Event Handler";
+	}
+
+}
